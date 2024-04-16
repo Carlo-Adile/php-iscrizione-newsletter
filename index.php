1622088a5515ac4b1b094055604006a16f95e71b
Carlo-Adile <adile.carlo.a@gmailcom>
2024-04-16 17:09:41 +0200

    add template

--- index.php
+++ index.php
@@ -1,18 +1,211 @@
-<?php 
+<?php
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Newsletter</title>
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-  
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="container">
+        <a href="" class="navbar-brand">Newsletter</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a href="" class="nav-link active" aria-current="page">Home
+                <span class="visually-hidden">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <main>
+    <!-- jumbotron -->
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Welcome to my site</h1>
+        <p class="col-md-8 fs-4">
+          Lorem, ipsum dolor
+        </p>
+        <button class="btn btn-primary btn-lg" type="button">
+          Browse me
+        </button>
+      </div>
+    </div>
+
+    <section class="posts my-5">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/400/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/400/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/400/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/400/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/400/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/400/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/400/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/400/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="newsletter bg-secondary text-white py-4">
+      <div class="container">
+        <div class="col-6">
+          <h3>Newsletter</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quos, sequi fugiat consectetur consequuntur
+            quisquam cumque alias saepe adipisci distinctio temporibus expedita delectus harum accusamus impedit atque
+            sunt minus ut?
+          </p>
+        </div>
+        
+        <form action="" method="get" class="d-flex align-items-center justify-content-center">
+          <div class="">
+            <label for="email" class="form-label">Email</label>
+            <input type="text" class="form-control rounded-0" name="email" id="email" aria-describedby="helpId" placeholder="">
+            <small id="emailHelper" class="form-text text-muted">Type your email address</small>
+          </div>
+          <button type="submit" class="btn btn-dark rounded-0">Subscribe</button>
+        </form>
+      </div>
+
+    </section>
+
+  </main>
+
+  <footer class="py-4 bg-dark text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h3>Company</h3>
+          <ul class="list-unstyled">
+            <li><a href="">Lorem.</a></li>
+            <li><a href="">Reiciendis.</a></li>
+            <li><a href="">Incidunt!</a></li>
+            <li><a href="">Quasi.</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>Company</h3>
+          <ul class="list-unstyled">
+            <li><a href="">Lorem.</a></li>
+            <li><a href="">Reiciendis.</a></li>
+            <li><a href="">Incidunt!</a></li>
+            <li><a href="">Quasi.</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>Quick links</h3>
+          <ul class="list-unstyled">
+            <li><a href="">Lorem.</a></li>
+            <li><a href="">Reiciendis.</a></li>
+            <li><a href="">Incidunt!</a></li>
+            <li><a href="">Quasi.</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>Legal</h3>
+          <ul class="list-unstyled">
+            <li><a href="">Lorem.</a></li>
+            <li><a href="">Reiciendis.</a></li>
+            <li><a href="">Incidunt!</a></li>
+            <li><a href="">Quasi.</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
 </body>
+
 </html>
